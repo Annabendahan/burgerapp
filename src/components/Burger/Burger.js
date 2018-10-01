4846b728f1ed6061889990dc14b98ea0e843389b
Anna Bendahan <an.bendahan@gmail.com>
2018-10-01 15:11:35 +0200

    initial commi

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+  .map( igKey => {
+    console.log(Object.keys(props.ingredients)); // (salad, meat, cheese, bacon)
+    console.log (igKey); // salad / meat / cheese / bacon
+    console.log(props.ingredients[igKey]); // 0, 1 , 2 , 3 .... nb de salad, bacon..
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      console.log([...Array(props.ingredients[igKey])]); // ( _, _ )
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el)
+    }, []);
+
+  console.log(transformedIngredients);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p> Please start adding ingredients! </p>
+    };
+
+
+  return (
+    <div className= {classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+    );
+};
+
+export default burger;
